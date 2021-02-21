3cd803bfdf3747de0b9592d644ad85631b251ba1
Matija Turčec <turcec.matija@gmail.com>
2021-02-21 15:32:48 +0100

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { AddNew } from './routes/AddNew';
 import { Home } from './routes/Home';
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/addnew">
+          <AddNew />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 

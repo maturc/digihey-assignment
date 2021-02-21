c193ad2f7bba6b51a13be41b87e8ee0f157f45c7
Matija Turčec <turcec.matija@gmail.com>
2021-02-21 15:05:44 +0100

    basic settup of the header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export function Header() {
+  return (
+    <header>
+      <h1>
+        Transformer Management
+      </h1>
+      <div>
+        Search
+      </div>
+    </header>
+  );
+}

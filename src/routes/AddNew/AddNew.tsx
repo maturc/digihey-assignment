bb089f0e5228eb77be898bbf3d5fb6427ab07f9f
Matija Turčec <turcec.matija@gmail.com>
2021-02-21 23:19:24 +0100

    expaned the add new page

--- src/routes/AddNew/AddNew.tsx
+++ src/routes/AddNew/AddNew.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Header } from '../../components/Header';
+import { TransformerForm } from '../../components/TransformerForm';
+import { VehicleTree } from '../../utils/VehicleTree';
 
 export function AddNew() {
+  const [vehicleTypes, setVehicleTypes] = useState<VehicleTree>();
+
+  useEffect(() => {
+    const fetchTypes = async () => {
+      const response = await fetch('http://localhost:3004/vehicleTypes');
+      const json = await response.json();
+      const vehicleTree = new VehicleTree();
+      vehicleTree.addFromArray(json);
+      setVehicleTypes(vehicleTree);
+    };
+    fetchTypes();
+  }, []);
   return (
     <>
-      HEllo world
+      <Header />
+      <TransformerForm vehicleTypes={vehicleTypes as VehicleTree} />
     </>
   );
 }

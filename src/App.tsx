42fa4eb8a90b01c689a77c3a555d6240582de44e
Matija Turčec <turcec.matija@gmail.com>
2021-02-22 10:35:55 +0100

    added new route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { AddNew } from './routes/AddNew';
+import { Details } from './routes/Details';
 import { Home } from './routes/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path="/edit/:id" component={Details} />
         <Route path="/add">
           <AddNew />
         </Route>

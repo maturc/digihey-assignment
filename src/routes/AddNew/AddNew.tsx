696051aa0f19eb1b973964c3e5799d9a1e276d1a
Matija Turčec <turcec.matija@gmail.com>
2021-02-22 10:34:27 +0100

    moved the data fetching into the form component

--- src/routes/AddNew/AddNew.tsx
+++ src/routes/AddNew/AddNew.tsx
@@ -4,22 +4,10 @@ import { TransformerForm } from '../../components/TransformerForm';
 import { VehicleTree } from '../../utils/VehicleTree';
 
 export function AddNew() {
-  const [vehicleTypes, setVehicleTypes] = useState<VehicleTree>();
-
-  useEffect(() => {
-    const fetchTypes = async () => {
-      const response = await fetch('http://localhost:3004/vehicleTypes');
-      const json = await response.json();
-      const vehicleTree = new VehicleTree();
-      vehicleTree.addFromArray(json);
-      setVehicleTypes(vehicleTree);
-    };
-    fetchTypes();
-  }, []);
   return (
     <>
       <Header />
-      <TransformerForm vehicleTypes={vehicleTypes as VehicleTree} />
+      <TransformerForm />
     </>
   );
 }

cc982fc160a3b52087eb7c9a077715ba49c39d25
Matija Turčec <turcec.matija@gmail.com>
2021-02-22 10:35:04 +0100

    works now

--- src/routes/Details/Details.tsx
+++ src/routes/Details/Details.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import { useEffect, useState } from "react";
+import { TransformerForm } from "../../components/TransformerForm";
 
-export function Details() {
+export function Details(props: any) {
+  const id = props.match.params.id;
+  
+  const [transformer, setTransformer] = useState<ITransformer>();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchTransformers = async () => {
+      try {
+        const response = await fetch(`http://localhost:3004/transformers/${id}`);
+        const json = await response.json();
+        setTransformer(json);
+        setIsLoading(false);
+      } catch (err) {
+        console.log(err); //EXPAND
+      }
+    };
+    fetchTransformers();
+  }, []);
   return (
     <>
-      HEllo world
+      { isLoading ? <h1>Loading</h1> : <TransformerForm initialTransformer={transformer} /> }
     </>
   );
 }

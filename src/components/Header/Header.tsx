ff34e982d9099730b62ea614d5791fb4292bdcd6
Matija Turčec <turcec.matija@gmail.com>
2021-02-22 14:43:25 +0100

    fixed logo bug

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './header.css';
+import logo from '../../assets/Autobot.svg';
 
 export function Header() {
   return (
     <header className="header">
       <Link to="/" className="header__link">
-        <img src='./Autobot.svg' className="header__logo" />
+        <img src={logo} className="header__logo" alt="logo" />
         <h1 className="header__text">
           Transformer Management
         </h1>
